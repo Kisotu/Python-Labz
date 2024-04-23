827fb798936afaad820a0eed48218f5e2cd2af64
Oloishorua <jacoboloishorua@gmail.com>
2024-04-23 18:27:18 +0300

    creates a list

---
+++ data-structures/lists/create_list.py
@@ -0,0 +1,17 @@
+# stores collection of data
+# create a python list, [] symbol used
+
+mylist = ["apple", "banana", "cocoa", "dew", "eat"]
+print(mylist)
+
+# access list items:
+print(mylist[0])
+print(mylist[3])
+
+# get length using len()
+print(len(mylist))
+
+# also cerated using the list() constructor, with double round brackets (())
+
+newlist = list(("fruit", "vegetable", True, 78, "Hello"))
+print(newlist)
