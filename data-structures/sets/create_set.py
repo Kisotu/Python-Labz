5796b989ae7bb5648aabda2327d52bc0c4688528
Oloishorua <jacoboloishorua@gmail.com>
2024-05-02 00:40:34 +0300

    shows how to create a set

---
+++ data-structures/sets/create_set.py
@@ -0,0 +1,27 @@
+# sets are collection of data items that are unordered, unchangeable and unindexed
+# items are unchangeable, but can remove or add new items
+myset = {"apple", "jupiter", "earth"}
+print(myset)
+
+#sets do not allow duplicates
+#sets are unordered therefore not indexed
+print(len(myset))
+
+
+
+
+#can be of any data type
+set1 = {"apple", "banana", "cherry"}
+set2 = {1, 5, 7, 9, 3}
+set3 = {True, False}
+set4 = {"abc", 34, True, 40, "male"}
+print(set3)
+print(set4)
+
+
+
+
+# can also be created using the set() constructor
+set5 = set(("apple", "mango", "ugali", "milk", "banana"))
+print(type(set5))
+print(set5)
