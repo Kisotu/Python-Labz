44c2d8dff5791ba9c9236aaa9de196e6437c4117
Oloishorua <jacoboloishorua@gmail.com>
2024-06-08 23:48:31 +0300

    test commit

--- Flask/tuts/test.py
+++ Flask/tuts/test.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def user():
 	first_name = "Jake"
-	return render_template('index.html', first_name = first_name)
+	age = 89
+	return render_template('index.html', first_name = first_name, age = age)
 
 if __name__ == "__main__":
 	app.run()
